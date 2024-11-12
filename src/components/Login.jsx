6df3fc09a92b3150ff17628a40f1ab3ecfd77a79
Nahid Hasan <nahidn228@gmail.com>
2024-11-12 21:18:20 +0600

    user signIn with email pass done

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,27 @@
+import { useContext } from "react";
 import { FaGithub, FaTwitter } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
+import { AuthContext } from "../provider/AuthProvider";
 
 const Login = () => {
+  const { signIn } = useContext(AuthContext);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+    console.log(email, password);
+
+    signIn(email, password)
+      .then((result) => {
+        console.log(result.user);
+      })
+      .catch((err) => {
+        console.log("ERROR ", err.message);
+      });
+  };
+
   return (
     <div className="hero  min-h-screen">
       <div className="hero-content flex-col lg:flex-row">
@@ -13,7 +33,7 @@ const Login = () => {
           />
         </div>
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-          <form className="card-body">
+          <form onSubmit={handleLogin} className="card-body">
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
@@ -21,6 +41,7 @@ const Login = () => {
               <input
                 type="email"
                 placeholder="email"
+                name="email"
                 className="input input-bordered"
                 required
               />
@@ -31,6 +52,7 @@ const Login = () => {
               </label>
               <input
                 type="password"
+                name="password"
                 placeholder="password"
                 className="input input-bordered"
                 required
--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,20 +1,24 @@
 /* eslint-disable react/prop-types */
 
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import { createContext } from "react";
 import { auth } from "./../firebase/firebase.init";
 export const AuthContext = createContext(null);
 
 const AuthProvider = ({ children }) => {
-  const name = "halim dalim";
-
   const createUser = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
+  };
+  const signIn = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
   };
 
   const authInfo = {
     createUser,
-    name,
+    signIn,
   };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
