03f25377296e9fd4941135f8f8fa8315a289fae7
Nahid Hasan <nahidn228@gmail.com>
2024-11-12 21:29:39 +0600

    Get the currently signed-in user done

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -2,6 +2,7 @@
 
 import {
   createUserWithEmailAndPassword,
+  onAuthStateChanged,
   signInWithEmailAndPassword,
 } from "firebase/auth";
 import { createContext } from "react";
@@ -15,6 +16,14 @@ const AuthProvider = ({ children }) => {
   const signIn = (email, password) => {
     return signInWithEmailAndPassword(auth, email, password);
   };
+
+  onAuthStateChanged(auth, (currentUser) => {
+    if (currentUser) {
+      console.log("currently Logged in", currentUser);
+    } else {
+      console.log("No user logged in");
+    }
+  });
 
   const authInfo = {
     createUser,
